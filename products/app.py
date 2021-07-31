1c01b76ca5d98350b601c402e6a54b8c428ed1f6
Karim Saad <karim.saad@okendo.io>
2021-07-31 13:56:12 +1000

    initial setup

---
+++ products/app.py
@@ -0,0 +1,39 @@
+'''
+Entry file for Lambda
+'''
+import json
+
+import boto3
+
+
+def lambda_handler(event, context):
+    '''Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    '''
+
+    products_table = boto3.resource('dynamodb').Table('products')
+    products = products_table.scan()['Items']
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'products': products
+        }),
+    }
