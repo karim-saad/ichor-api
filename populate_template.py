bf625cfd2c285bc213e5f6ef859c21d5ad623ad7
Karim Saad <karim.saad@okendo.io>
2021-08-23 21:51:54 +1000

    populate script adjustments

--- populate_template.py
+++ populate_template.py
@@ -2,15 +2,19 @@
 Script to populate template.yml file with sensitive environment variables
 '''
 import json
+
+
 if __name__ == '__main__':
-    empty_template = open('empty_template.yml').read()
-    env_vars = json.loads(open('.params.json').read())
+    with open('empty_template.yml') as f:
+        empty_template = f.read()
+    with open('params.json') as f:
+        env_vars = json.loads(f.read())
 
-    populated_template = empty_template
-    populated_template += '\nParameters:\n'
-    for env_var in env_vars:
-        populated_template += f'  {env_var}:\n'
+    populated_template = empty_template + '\nParameters:\n'
+    for key, value in env_vars.items():
+        populated_template += f'  {key}:\n'
         populated_template += '    Type: String\n'
-        populated_template += f'    Default: {env_vars[env_var]}\n'
+        populated_template += f'    Default: {value}\n'
 
-    open('template.yml', 'w').write(populated_template)
+    with open('template.yml', 'w') as f:
+        f.write(populated_template)
